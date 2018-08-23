cf5e731f1fb40af5a0623904c55aea5bbd47121f
Andreas Neuenschwander <uparix@users.noreply.github.com>
2018-08-23 07:27:20 +0200

    excercise 1.5 complete

--- src/sicp/excercise1.clj
+++ src/sicp/excercise1.clj
@@ -3,3 +3,13 @@
 (def a 3)
 
 (def b (+ a 1))
+
+(defn squareSum [a b c] (+ (* (max a b) (max a b)) (* (max a c) (max a c))))
+
+(defn a-plus-abs-b [a b] ((if (> b 0) + -) a b))
+
+(defn p [] (p))
+
+(defn test-p [x y] (if (= x 0) 0 y))
+
+(defn (new-if [predi then-clause else-clause] (cond predi then-clause :else else-clause)))
--- test/sicp/excercise1_test.clj
+++ test/sicp/excercise1_test.clj
@@ -24,5 +24,26 @@
        )
 
 (facts "sicp excercise 1.3"
-       (fact "sum square of two larger numbers")
+       (fact "sum square of two larger numbers" (squareSum 4 5 0) => 41)
+       (fact "sum square of two larger numbers" (squareSum 4 0 5) => 41)
+       (fact "sum square of two larger numbers" (squareSum 4 3 5) => 41)
+       (fact "sum square of two larger numbers" (squareSum 1 2 3) => 13)
        )
+
+(fact "sicp excercise 1.4"
+      (fact "observe model 1" (a-plus-abs-b 0 0) => 0 )
+      (fact "observe model 2" (a-plus-abs-b 1 0) => 1 )
+      (fact "observe model 3" (a-plus-abs-b 1 1) => 2 )
+      (fact "observe model 4" (a-plus-abs-b -1 0) => -1 )
+      (fact "observe model 5" (a-plus-abs-b -1 1) => 0 )
+      (fact "observe model 6" (a-plus-abs-b -1 -1) => 0 )
+      (fact "observe model 7" (a-plus-abs-b -2 -1) => -1 )
+      )
+
+(fact "sicp excercise 1.5"
+      (fact "observe model 1" (test-p 0 (p)) => (throws StackOverflowError) )
+      )
+
+(fact "sicp excercise 1.6"
+      (fact "" (new-if (= 2 3) 0 5) => 0)
+      )
